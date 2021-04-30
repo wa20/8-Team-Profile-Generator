8aff71c4a9af55ca9968a12db9017c58ac453f60
wael ahmed <w.ahmed020@gmail.com>
2021-04-30 15:02:20 +0100

    app js code updates

--- app.js
+++ app.js
@@ -1,91 +1,94 @@
 console.log("Create Profile");
 
-const fs = require('fs');
-const util = require('util');
-const inquirer = require('inquirer');
+const fs = require("fs");
+const util = require("util");
+const inquirer = require("inquirer");
 
-const generateProfile = require('./utils/generateProfile')
+const generateProfile = require("./utils/generateProfile");
 
+const writeFileAsync = util.promisify(fs.writeFile);
 
-
+const writeToFile = (fileName, data) => {
+  writeFileAsync(fileName, data);
+};
 
 const profileGenerator = () => {
-    inquirer.prompt([
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "id",
+        message: "Enter profile ID number: ",
+        validate: function (userAnswer) {
+          if (userAnswer !== typeof Number) {
+            return console.log("Please enter a number");
+          }
+          return true;
+        },
+      },
 
-        {
-            type: "input",
-            name: "id",
-            message: "Enter profile ID number: ",
-            validate: function (userAnswer) {
-              if (userAnswer !== typeof Number) {
-                return console.log("Please enter a number");
-              }
-              return true;
-            },
-          },
+      {
+        type: "list",
+        name: "Icon",
+        message: "Choose your license for your project: ",
+        choices: ["Manager", "Engineer", "Intern"],
+        validate: function (userAnswer) {
+          if (userAnswer === "") {
+            return console.log("Please make choice");
+          }
+          return true;
+        },
+      },
 
-          {
-            type: "list",
-            name: "Icon",
-            message: "Choose your license for your project: ",
-            choices: ["None", "Manager", "Engineer", "Intern"],
-          },
+      {
+        type: "input",
+        name: "name",
+        message: "Enter your name: ",
+        validate: function (userAnswer) {
+          if (userAnswer === "") {
+            return console.log("Please input user name");
+          }
+          return true;
+        },
+      },
 
-        
-        {
-            type: "input",
-            name: "name",
-            message: "Enter your name: ",
-            validate: function (userAnswer) {
-              if (userAnswer === "") {
-                return console.log("Please input user name");
-              }
-              return true;
-            },
-          },
+      {
+        type: "input",
+        name: "gitHubLink",
+        message: "Enter link to your github profile: ",
+        validate: function (userAnswer) {
+          if (userAnswer === "") {
+            return console.log("Please input link to github profile");
+          }
+          return true;
+        },
+      },
 
+      {
+        type: "input",
+        name: "email",
+        message: "Enter Project Title: ",
+        validate: function (userAnswer) {
+          if (userAnswer === "") {
+            return console.log("Please input Project Title");
+          }
+          return true;
+        },
+      },
 
-          {
-            type: "input",
-            name: "gitHubLink",
-            message: "Enter link to your github profile: ",
-            validate: function (userAnswer) {
-              if (userAnswer === "") {
-                return console.log("Please input link to github profile");
-              }
-              return true;
-            },
-          },  
-
-
-          {
-            type: "input",
-            name: "email",
-            message: "Enter Project Title: ",
-            validate: function (userAnswer) {
-              if (userAnswer === "") {
-                return console.log("Please input Project Title");
-              }
-              return true;
-            },
-          },
-
-
-          {
-            type: "input",
-            name: "dateJoined",
-            message: "Enter date joined mm/yyyy: ",
-            validate: function (userAnswer) {
-              if (userAnswer === "") {
-                return console.log("Please input Project Title");
-              }
-              return true;
-            },
-          },
-
-
-
-
-
+      {
+        type: "input",
+        name: "dateJoined",
+        message: "Enter date joined mm/yyyy: ",
+        validate: function (userAnswer) {
+          if (userAnswer === "") {
+            return console.log("Please input Project Title");
+          }
+          return true;
+        },
+      },
     ])
-}
+    .then((data) => writeToFile("...", generateProfile(data)));
+};
+
+profileGenerator();
