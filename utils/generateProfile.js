d2935665607f925d60c1643df3e9bc61374c763b
wael ahmed <w.ahmed020@gmail.com>
2021-04-30 13:58:48 +0100

    code updates js file

--- utils/generateProfile.js
+++ utils/generateProfile.js
@@ -1,42 +1,60 @@
+
+function renderIcon(Icon) {
+    if (teamMemebr === "Manager") {
+      return "chess king icon";
+    } else
+    if (teamMemebr === "Engineer") {
+        return "chess bishop icon";
+    } else 
+    if (teamMemebr === "Intern") {
+        return "chess pawn icon";
+    }
+  }
+
+
+
+  
+
+
 function generateProfile(data) {
   return;
 
-  `
-    <li class="card">
+`
+<li class="card">
     <div class="content">
-      <div class="right floated meta">ID:</div>
-      <i class="chess king icon ${data.icon}"></i>
-     
+    <div class="right floated meta">ID: ${data.id}</div>
+    <i class="${renderIcon(data.icon)}"></i>
+        
     </div>
 
     <div class="image">
-      <img src="./assets/male_avatar_sq.svg" />
+    <img src="./assets/male_avatar_sq.svg" />
     </div>
-<div class="content">
-      <div class="header">${data.name}</div>
-     
+    <div class="content">
+    <div class="header">${data.name}</div>
+        
 
-  <div class="ui middle aligned animated divided list">
-      <div class="item">
-          <img class="ui avatar image" src="./assets/email_icon.png" />
-          <div class="content">
-            <a class="header" href="${data.email}" target="_blank">${data.email}</a>
-          </div>
-      </div>
-        <div class="item">
-          <img class="ui avatar image" src="./assets/github-512.png" />
-          <div class="content">
-            <a class="header" href="${data.gitHubLink}" target="_blank">${data.gitHubLink}</a>
-          </div>
-      </div>
-  </div>
-</div>
+    <div class="ui middle aligned animated divided list">
+    <div class="item">
+    <img class="ui avatar image" src="./assets/email_icon.png" />
+    <div class="content">
+    <a class="header" href="${data.email}" target="_blank">${data.email}</a>
+    </div>
+    </div>
+    <div class="item">
+    <img class="ui avatar image" src="./assets/github-512.png" />
+    <div class="content">
+    <a class="header" href="${data.gitHubLink}" target="_blank">${data.gitHubLink}</a>
+    </div>
+    </div>
+    </div>
+    </div>
 
- <div class="extra content">
+    <div class="extra content">
     <span class="right floated">${data.dateJoined}</span>
-     </div>
-        </li>
-    `;
+    </div>
+</li>
+`;
 }
 
 module.exports = generateMarkdown;
