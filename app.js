3bdf0bcd508610d1059e0c0b56f1f2b82dea9cb6
wael ahmed <w.ahmed020@gmail.com>
2021-05-02 13:39:05 +0100

    app js code updates

--- app.js
+++ app.js
@@ -78,24 +78,180 @@ const generateManager = () => { //user is first prompted to input manager detail
         },
       },
     ]). then((data) =>{
-        const managerCard = new generateProfile({
-            
+        const managerCard = new generateProfile({ 
             id: data.id,
             icon: data.icon,
             name: data.name,
             email: data.email,
             gitHubLink: data.gitHubLink,
-            dateJoined: data.dataJoined
-            
-        }) 
-        
+            dateJoined: data.dataJoined   
+        })
+            profile.push(managerCard);
+
     })
     
 };
+
+
+const generateEngineer = () => {
+    inquirer.prompt([ {
+        type: "list",
+        name: "Icon",
+        message: "Select Enginer to begin or Done to end: ",
+        choices: ["Engineer","Done"],
+        validate: function (userAnswer) {
+          if (userAnswer === "") {
+            return console.log("Please make choice");
+          }
+          return true;
+        },
+      },
+
+      {
+        type: "input",
+        name: "id",
+        message: "Enter profile ID number: ",
+        validate: function (userAnswer) {
+          if (userAnswer !== typeof Number) {
+            return console.log("Please enter a number");
+          }
+          return true;
+        },
+      },
+
+      {
+        type: "input",
+        name: "name",
+        message: "Enter your name: ",
+        validate: function (userAnswer) {
+          if (userAnswer === "") {
+            return console.log("Please input user name");
+          }
+          return true;
+        },
+      },
+
+      {
+        type: "input",
+        name: "gitHubLink",
+        message: "Enter link to your github profile: ",
+        validate: function (userAnswer) {
+          if (userAnswer === "") {
+            return console.log("Please input link to github profile");
+          }
+          return true;
+        },
+      },
+
+      {
+        type: "input",
+        name: "dateJoined",
+        message: "Enter date joined mm/yyyy: ",
+        validate: function (userAnswer) {
+          if (userAnswer === "") {
+            return console.log("Please input Project Title");
+          }
+          return true;
+        },
+      },
+        
+    ]). then((data) =>{
+        const engineerCard = new generateProfile({ 
+            id: data.id,
+            icon: data.icon,
+            name: data.name,
+            email: data.email,
+            gitHubLink: data.gitHubLink,
+            dateJoined: data.dataJoined   
+        })
+            profile.push(engineerCard);
+    })
+
+}
+
+const generateIntern = () => {
+    inquirer.prompt([ {
+        type: "list",
+        name: "Icon",
+        message: "Select Enginer to begin or Done to end: ",
+        choices: ["Intern","Done"],
+        validate: function (userAnswer) {
+          if (userAnswer === "") {
+            return console.log("Please make choice");
+          }
+          return true;
+        },
+      },
+
+      {
+        type: "input",
+        name: "id",
+        message: "Enter profile ID number: ",
+        validate: function (userAnswer) {
+          if (userAnswer !== typeof Number) {
+            return console.log("Please enter a number");
+          }
+          return true;
+        },
+      },
+
+      {
+        type: "input",
+        name: "name",
+        message: "Enter your name: ",
+        validate: function (userAnswer) {
+          if (userAnswer === "") {
+            return console.log("Please input user name");
+          }
+          return true;
+        },
+      },
+
+      {
+        type: "input",
+        name: "gitHubLink",
+        message: "Enter link to your github profile: ",
+        validate: function (userAnswer) {
+          if (userAnswer === "") {
+            return console.log("Please input link to github profile");
+          }
+          return true;
+        },
+      },
+
+      {
+        type: "input",
+        name: "dateJoined",
+        message: "Enter date joined mm/yyyy: ",
+        validate: function (userAnswer) {
+          if (userAnswer === "") {
+            return console.log("Please input Project Title");
+          }
+          return true;
+        },
+      },
+        
+    ]). then((data) =>{
+        const engineerCard = new generateProfile({ 
+            id: data.id,
+            icon: data.icon,
+            name: data.name,
+            email: data.email,
+            gitHubLink: data.gitHubLink,
+            dateJoined: data.dataJoined   
+        })
+            profile.push(engineerCard);
+    })
+
+}
+
+
+
+
 
 profileGenerator();
 
 
 
 
-.then((data) => writeToFile("...", generateManager(data)));
+// .then((data) => writeToFile("...", generateManager(data)));
