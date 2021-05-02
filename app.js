d0099e95d7236a7462a4f1869d54fcd571cb4a69
wael ahmed <w.ahmed020@gmail.com>
2021-05-02 17:29:50 +0100

    app js file updates

--- app.js
+++ app.js
@@ -40,8 +40,14 @@ const generateManager = () => { //user is first prompted to input manager detail
 
       {
         type: "input",
-        name: "gitHubLink",
-        message: "Enter link to your github profile: ",
+        name: "email",
+        message: "Enter your email: ",
+      },
+
+      {
+        type: "input",
+        name: "phoneNumber",
+        message: "Enter Phone Number: ",
       },
 
       {
@@ -51,9 +57,10 @@ const generateManager = () => { //user is first prompted to input manager detail
       },
     ]). then((data) =>{
         const manager = new Manager(
-          data.name, 
           data.id, 
+          data.name, 
           data.email, 
+          dat.phoneNumber,
           data.dateJoined
           )
             profile.push(manager);
@@ -70,60 +77,31 @@ const generateEngineer = () => {
         name: "Icon",
         message: "Select Enginer to begin or Done to end: ",
         choices: ["Engineer","Done"],
-        validate: function (userAnswer) {
-          if (userAnswer === "") {
-            return console.log("Please make choice");
-          }
-          return true;
-        },
       },
 
       {
         type: "input",
         name: "id",
         message: "Enter profile ID number: ",
-        // validate: function (userAnswer) {
-        //   if (userAnswer !== typeof Number) {
-        //     return console.log("Please enter a number");
-        //   }
-        //   return true;
-        // },
+       
       },
 
       {
         type: "input",
         name: "name",
         message: "Enter your name: ",
-        validate: function (userAnswer) {
-          if (userAnswer === "") {
-            return console.log("Please input user name");
-          }
-          return true;
-        },
       },
 
       {
         type: "input",
         name: "gitHubLink",
         message: "Enter link to your github profile: ",
-        validate: function (userAnswer) {
-          if (userAnswer === "") {
-            return console.log("Please input link to github profile");
-          }
-          return true;
-        },
       },
 
       {
         type: "input",
         name: "dateJoined",
         message: "Enter date joined mm/yyyy: ",
-        validate: function (userAnswer) {
-          if (userAnswer === "") {
-            return console.log("Please input Project Title");
-          }
-          return true;
-        },
       },
         
     ]). then((data) =>{
