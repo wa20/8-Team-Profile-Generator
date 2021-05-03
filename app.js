8867cb60bf2e39343307db592d925533c91c6f60
wael ahmed <w.ahmed020@gmail.com>
2021-05-03 14:14:25 +0100

    app js code updates

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const util = require("util");
 const inquirer = require("inquirer");
 const generateProfile = require("./utils/generateProfile");
-const Manager = require("./lib/Manager")
+const Manager = require("./lib/Manager");
 
 // const writeFileAsync = util.promisify(fs.writeFile);
 
@@ -15,188 +15,189 @@ const htmlPath = path.join(__dirname, "team.html");
 const profile = [];
 
 const profileGenerator = () => {
-const generateManager = () => { //user is first prompted to input manager details for profile card
-  inquirer
-    .prompt([
+  const generateManager = () => {
+    //user is first prompted to input manager details for profile card
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "Icon",
+          message: "Select Manager to begin: ",
+          choices: ["Manager"],
+        },
 
         {
-        type: "list",
-        name: "Icon",
-        message: "Select Manager to begin: ",
-        choices: ["Manager"],
-      },
+          type: "input",
+          name: "id",
+          message: "Enter profile ID number: ",
+        },
 
-      {
-        type: "input",
-        name: "id",
-        message: "Enter profile ID number: ",
-      },
+        {
+          type: "input",
+          name: "name",
+          message: "Enter name: ",
+        },
 
-      {
-        type: "input",
-        name: "name",
-        message: "Enter name: ",
-      },
+        {
+          type: "input",
+          name: "email",
+          message: "Enter Email: ",
+        },
 
-      {
-        type: "input",
-        name: "email",
-        message: "Enter Email: ",
-      },
+        {
+          type: "input",
+          name: "phoneNumber",
+          message: "Enter Phone Number: ",
+        },
 
-      {
-        type: "input",
-        name: "phoneNumber",
-        message: "Enter Phone Number: ",
-      },
-
-      {
-        type: "input",
-        name: "dateJoined",
-        message: "Enter date joined mm/yyyy: ",
-      },
-    ]). then((data) =>{
+        {
+          type: "input",
+          name: "dateJoined",
+          message: "Enter date joined mm/yyyy: ",
+        },
+      ])
+      .then((data) => {
         const manager = new Manager(
-          data.id, 
-          data.name, 
-          data.email, 
-          dat.phoneNumber,
+          data.id,
+          data.name,
+          data.email,
+          data.phoneNumber,
           data.dateJoined
-          )
-            profile.push(manager);
-            addTeamMember()
+        );
+        profile.push(manager);
+        addTeamMember();
+      });
+  };
 
-    })
-    
+  const generateEngineer = () => {
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "Icon",
+          message: "Select Engineer to begin: ",
+          choices: ["Engineer"],
+        },
+
+        {
+          type: "input",
+          name: "id",
+          message: "Enter profile ID number: ",
+        },
+
+        {
+          type: "input",
+          name: "name",
+          message: "Enter name: ",
+        },
+
+        {
+          type: "input",
+          name: "email",
+          message: "Enter Email: ",
+        },
+
+        {
+          type: "input",
+          name: "gitHubLink",
+          message: "Enter link to github profile: ",
+        },
+
+        {
+          type: "input",
+          name: "dateJoined",
+          message: "Enter date joined mm/yyyy: ",
+        },
+      ])
+      .then((data) => {
+        const engineer = new Engineer(
+          data.id,
+          data.name,
+          data.email,
+          data.gitHubLink,
+          data.dateJoined
+        );
+        profile.push(engineer);
+        addTeamMember();
+      });
+  };
+
+  const generateIntern = () => {
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "Icon",
+          message: "Select Intern to begin: ",
+          choices: ["Intern"],
+        },
+
+        {
+          type: "input",
+          name: "id",
+          message: "Enter profile ID number: ",
+        },
+
+        {
+          type: "input",
+          name: "name",
+          message: "Enter name: ",
+        },
+
+        {
+          type: "input",
+          name: "gitHubLink",
+          message: "Enter link to github profile: ",
+        },
+
+        {
+          type: "input",
+          name: "dateJoined",
+          message: "Enter date joined mm/yyyy: ",
+        },
+      ])
+      .then((data) => {
+        const intern = new Intern(
+          data.id,
+          data.name,
+          data.email,
+          data.gitHubLink,
+          data.dateJoined
+        );
+        profile.push(intern);
+        addTeamMember();
+      });
+  };
+
+  const addTeamMember = () => {
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "addTeamMember",
+          message: "Would you like to add another team member: ",
+          choices: ["Engineer", "Intern", "Done"],
+        },
+      ])
+      .then((data) => {
+        if (data.addTeamMember === "Engineer") {
+          generateEngineer();
+        } else if (data.addTeamMember === "Intern") {
+          generateIntern();
+        } else {
+          console.log(profile);
+          buildHtml();
+        }
+      });
+  };
+
+  function buildHtml() {
+    fs.writeFileSync(htmlPath, generateProfile(profile), "utf-8");
+  }
+
+  generateManager();
 };
 
-
-const generateEngineer = () => {
-    inquirer.prompt([ {
-        type: "list",
-        name: "Icon",
-        message: "Select Engineer to begin: ",
-        choices: ["Engineer","Done"],
-      },
-
-      {
-        type: "input",
-        name: "id",
-        message: "Enter profile ID number: ",
-       
-      },
-
-      {
-        type: "input",
-        name: "name",
-        message: "Enter name: ",
-      },
-
-      {
-        type: "input",
-        name: "gitHubLink",
-        message: "Enter link to github profile: ",
-      },
-
-      {
-        type: "input",
-        name: "dateJoined",
-        message: "Enter date joined mm/yyyy: ",
-      },
-        
-    ]). then((data) =>{
-          const engineer = new Engineer(
-            data.id, 
-            data.name, 
-            data.gitHubLink, 
-            data.dateJoined
-            )
-              profile.push(manager);
-              addTeamMember()
-  
-      })
-
-}
-
-const generateIntern = () => {
-    inquirer.prompt([ {
-        type: "list",
-        name: "Icon",
-        message: "Select Intern to begin or Done to end: ",
-        choices: ["Intern"],
-      },
-
-      {
-        type: "input",
-        name: "id",
-        message: "Enter profile ID number: ",
-      },
-
-      {
-        type: "input",
-        name: "name",
-        message: "Enter name: ",
-      },
-
-      {
-        type: "input",
-        name: "gitHubLink",
-        message: "Enter link to github profile: ",
-      },
-
-      {
-        type: "input",
-        name: "dateJoined",
-        message: "Enter date joined mm/yyyy: ",
-      },
-        
-    ]). then((data) =>{
-      const engineer = new Engineer(
-        data.id, 
-        data.name, 
-        data.gitHubLink, 
-        data.dateJoined
-        )
-          profile.push(manager);
-          addTeamMember()
-
-    })
-
-}
-
-const addTeamMember = () => {
-    inquirer.prompt ([
-        {
-        type: "list",
-        name: "addTeamMember",
-        message: "Would you like to add another team member: ",
-        choices: ["Engineer","Intern","Done"],
-    }
-]) .then((data) => {
-    if (data.addTeamMember === "Engineer"){
-         generateEngineer()
-    } else
-    if(data.addTeamMember === "Intern"){
-         generateIntern()
-    } else {
-        console.log(profile)
-        buildHtml()
-    }
-})
-}
-
-function buildHtml(){
-
-fs.writeFileSync(htmlPath, generateProfile(profile), "utf-8")
-}
-
-generateManager();
-}
-
-
 profileGenerator();
-
-
-
 
 // .then((data) => writeToFile("...", generateManager(data)));
